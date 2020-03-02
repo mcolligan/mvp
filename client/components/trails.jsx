e505b1929c83f4a6a8a8a3aa9ad64ed9a4b75ebe
mcolligan <mcolligan1@gmail.com>
2020-03-02 13:18:37 -0700

    merge and build db

--- client/components/trails.jsx
+++ client/components/trails.jsx
@@ -65,28 +65,28 @@ class Trails extends React.Component {
     return (
       <div className="container">
         <SearchBox searchClick={this.searchClick.bind(this)} />
-        <div className="container p-1">
-          <h4><em><u>Trails Near: {this.state.loc}</u></em></h4>
-          <div className="container" style={{ overflown: 'hidden' }}>
-            <div className="carousel slide" data-ride="carousel">
-              <div className="carousel-inner row" >
-                <div className="card-group d-flex flex-nowrap p-1">
-                  {this.state.trails.map((el, i) =>
-                    <div key={i} className="carousel item" style={sliderStyle}>
-                      <TrailsList key={i} info={el} />
-                    </div>
-                  )}
+          <div className="container p-1">
+            <h4><em><u>Trails Near: {this.state.loc}</u></em></h4>
+            <div className="container" style={{ overflown: 'hidden' }}>
+              <div className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner row" >
+                  <div className="card-group d-flex flex-nowrap p-1">
+                    {this.state.trails.map((el, i) =>
+                      <div key={i} className="carousel item" style={sliderStyle}>
+                        <TrailsList key={i} info={el} />
+                      </div>
+                    )}
+                  </div>
                 </div>
+                <a className="carousel-control-prev" role="button" data-slide="prev" onClick={this.prevClick}>
+                  <i className="fas fa-chevron-circle-left fa-2x"></i>
+                </a>
+                <a className="carousel-control-next" role="button" data-slide="prev" onClick={this.nextClick}>
+                  <i className="fas fa-chevron-circle-right fa-2x"></i>
+                </a>
               </div>
-              <a className="carousel-control-prev" role="button" data-slide="prev" onClick={this.prevClick}>
-                <i className="fas fa-chevron-circle-left fa-2x"></i>
-              </a>
-              <a className="carousel-control-next" role="button" data-slide="prev" onClick={this.nextClick}>
-                <i className="fas fa-chevron-circle-right fa-2x"></i>
-              </a>
             </div>
           </div>
-        </div>
       </div>
     )
   }
